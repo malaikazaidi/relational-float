b484db4e3da37a3a268b33d24e61ebc67cc2ef55
Malaika Zaidi <malaika.zaidi@mail.utoronto.ca>
2021-04-19 16:03:08 -0400

    changed to accomodate for 8bit frac

--- test-fp-pluso.rkt
+++ test-fp-pluso.rkt
@@ -11,12 +11,12 @@
         (test/fp-relation-r "-4 + -1 = ?" n5 : (fp-pluso negfour negone x) (x))
         (test/fp-relation-r "-4 + -70 = ?" n74 : (fp-pluso negfour negseventy x) (x))
         (test/fp-relation-r "-421 + -1 = ?" n422 : (fp-pluso neg421 negone x) (x))
-        (test/fp-relation-r "2.5 + 3 = ?" p5.5 : (fp-pluso p2.5 three x) (x))
-        (test/fp-relation-r "2.5 + 3.25 = ?" p5.75 : (fp-pluso p2.5 p3.25 x) (x))
-        (test/fp-relation-r "1.05 + 3.2325 = ?" p4.2825 : (fp-pluso p1.05 p3.2325 x) (x))
-        (test/fp-relation-r "-4 + -9 = ?" n13 :(fp-pluso negfour n9 x) (x))
-        (test/fp-relation-r "-10.12 + ? = -33.365" n23.245 : (fp-pluso n10.12 x n33.365) (x))
-        (test/fp-relation-r "? + -10.12 = -33.365" n23.245 : (fp-pluso x n10.12 n33.365) (x))
+        ;(test/fp-relation-r "2.5 + 3 = ?" p5.5 : (fp-pluso p2.5 three x) (x))
+       ; (test/fp-relation-r "2.5 + 3.25 = ?" p5.75 : (fp-pluso p2.5 p3.25 x) (x))
+        ;(test/fp-relation-r "1.05 + 3.2325 = ?" p4.2825 : (fp-pluso p1.05 p3.2325 x) (x))
+        ;(test/fp-relation-r "-4 + -9 = ?" n13 :(fp-pluso negfour n9 x) (x))
+       ; (test/fp-relation-r "-10.12 + ? = -33.365" n23.245 : (fp-pluso n10.12 x n33.365) (x))
+        ;(test/fp-relation-r "? + -10.12 = -33.365" n23.245 : (fp-pluso x n10.12 n33.365) (x))
 
         (test/fp-relation-r "42 + ? = 46" four : (fp-pluso fortytwo x p46) (x))
         (test/fp-relation-r "? + 42 = 46" four : (fp-pluso x fortytwo p46) (x))
@@ -47,10 +47,10 @@
         (test/fp-relation-r "? + 42 = 38" negfour : (fp-pluso x fortytwo p38) (x)) ;Takes really long, doesn't give a response
         (test/fp-relation-r "72 + ? = 70" ntwo : (fp-pluso seventytwo x seventy) (x)) ;Takes really long, doesn't give a response
         (test/fp-relation-r "(-4) + x = -3" one : (fp-pluso negfour x nthree) (x))
-        (test/fp-relation-r "x + (-4) = -3" one : (fp-pluso x negfour nthree) (x))
-        (test/fp-relation-r "4.5 + -7 = ?" n2.5 : (fp-pluso p4.5 n7 x) (x))
-        (test/fp-relation-r "(-5.5) + 3.25 = ?" n2.25 : (fp-pluso n5.5 p3.25 x) (x))
-        (test/fp-relation-r "5.5 + (-3.25) = ?" p2.25 : (fp-pluso p5.5 n3.25 x) (x))))
+        (test/fp-relation-r "x + (-4) = -3" one : (fp-pluso x negfour nthree) (x))))
+       ; (test/fp-relation-r "4.5 + -7 = ?" n2.5 : (fp-pluso p4.5 n7 x) (x))
+       ; (test/fp-relation-r "(-5.5) + 3.25 = ?" n2.25 : (fp-pluso n5.5 p3.25 x) (x))
+       ; (test/fp-relation-r "5.5 + (-3.25) = ?" p2.25 : (fp-pluso p5.5 n3.25 x) (x))))
 
 (displayln "Tests for same sign fp numbers")
 (run-tests equal-signs-test)
