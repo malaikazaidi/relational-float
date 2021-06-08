959bd34fb3354e03fa1489d1280657e57e957cf6
Lucas Sandre <lucasyyz@gmail.com>
2021-06-08 19:24:48 -0400

    Fixed reifier and adjusted test suite accordingly

--- float.rkt
+++ float.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(provide reify reify-exp reify-frac reify-sign get-sign get-frac get-exp POS-INFINITY NEG-INFINITY qNaN sNaN FP-ERR)
+(provide reify reify-exp reify-mantissa reify-sign get-sign get-mantissa get-exp POS-INFINITY NEG-INFINITY NaN FP-ERR)
 
 (define num '(;sign
               0 ; positive
@@ -20,11 +20,15 @@
 ; Constants for floats that aren't numbers
 (define POS-INFINITY 'positive-infinity)
 (define NEG-INFINITY 'negative-infinity)
-(define qNaN 'qNaN)
-(define sNaN 'sNaN)
+(define NaN 'NaN)
+
+
+(define PRECISION 16) ; number of bits in the mantissa.\
+(define EXPONENT-SHIFT 127)
 
 (define ZERO-EXP '()) ; For detecting denormalized numbers
 (define FULL-EXP '(1 1 1 1 1 1 1 1)) ; For detecting +/- infinity or NaN
+(define ZERO-MANTISSA (make-list PRECISION 0))
 
 #|
 (get-sign f)
@@ -41,53 +45,54 @@
 (define (get-exp f) (second f))
 
 #|
-(get-frac f)
+(get-mantissa f)
   f: A MKFP number
   Returns the 23 fractional bits of f. Also known as the significand or mantissa. 
 |#
-(define (get-frac f) (third f))
+(define (get-mantissa f) (third f))
 
 #|
-(inf? exp man)
-  exp: The exponent bits of a MKFP number.
-  man: The mantissa bits of a MKFP number.
+(inf? exponent man)
+  exponent: The exponent bits of a MKFP number.
+  mantissa: The mantissa bits of a MKFP number.
   Returns true if and only if all the exponent bits are 1 and if all the mantissa bits are 0. 
 |#
-(define (inf? exp man)       (and (equal? exp FULL-EXP) (equal? 0 (apply + (take man (- N_BITS 1))))))
+(define (inf? exponent mantissa)
+    (let* 
+        ([inf-exponent?  (equal? exponent FULL-EXP)]
+         [MSB-one?       (equal? 1 (last mantissa))]
+         [zero-fraction? (andmap zero? (drop-right mantissa 1))]) ; Is the mantissa all zeros after the MSB.
+
+        (and inf-exponent? MSB-one? zero-fraction?)))
 
 #|
-(qNaN? exp man)
-  exp: The exponent bits of a MKFP number.
-  man: The mantissa bits of a MKFP number.
+(NaN? exponent mantissa)
+  exponent: The exponent bits of a MKFP number.
+  mantissa: The mantissa bits of a MKFP number.
 
-  Returns true iff all the exponent bits are 1 and the most significant bit of the mantissa is 1.
+  Returns true iff all the exponent bits are 1 and the bits after the MSB are non-zero.
 |#
-(define (qNaN? exp man)       (and (equal? exp FULL-EXP) (equal? 1 (first (take-right man 2)))))
+(define (NaN? exponent mantissa)
+    (let* 
+        ([NaN-exponent? (equal? exponent FULL-EXP)]
+         [MSB-one?     (equal? 1 (last mantissa))]
+         [nonzero-fraction? (ormap 
+                                (lambda (int) (not (zero? int)))
+                                (drop-right mantissa 1))])
+
+         (and NaN-exponent? MSB-one? nonzero-fraction?))
+)
+
 
 #|
-(sNaN? exp man)
-  exp: The exponent bits of a MKFP number.
-  man: The mantissa bits of a MKFP number.
-
-  Returns true iff all the exponent bits are 1 and the most significant bit of the mantissa is 0 and there are other mantissa bits set to 1.
-|#
-(define (sNaN? exp man)       (and (equal? exp FULL-EXP) (not (equal? 0 (apply + man))) (equal? 0 (first (take-right man 2)))))
-
-#|
-(zero? exp man)
+(fp-zero? exponent mantissa)
   exp: The exponent bits of a MKFP number.
   man: The mantissa bits of a MKFP number.
   Returns true if and only if all the exponent bits are 0 and if all the mantissa bits are 0. 
 |#
-(define (zero? exp man)      (and (equal? exp ZERO-EXP) (equal? 0 (apply + man))))
-
-#|
-(subnormal? exp man)
-  exp: The exponent bits of a MKFP number.
-  man: The mantissa bits of a MKFP number.
-  Returns true if and only if all the exponent bits are 0 and if not all the mantissa bits are 0. 
-|#
-(define (subnormal? exp man) (and (equal? exp ZERO-EXP) (not (equal? 0 (apply + man)))))
+(define/match (fp-zero? exponent mantissa)
+    [((list) ZERO-MANTISSA) #t]
+    [(_ _) #f])
 
 #|
 (reify mkfp)
@@ -96,75 +101,79 @@
 |#
 (define (reify mkfp)
   (let* ([sign       (reify-sign (get-sign mkfp))]
-         [man        (get-frac mkfp)]
+         [mantissa   (get-mantissa mkfp)]
          [stored-exp (get-exp mkfp)])
     (cond
-      [(inf? stored-exp man) (cond
-                               [(equal? 1 sign) POS-INFINITY]
-                               [(equal? -1 sign) NEG-INFINITY])]
-      [(qNaN? stored-exp man) qNaN]
-      [(sNaN? stored-exp man) sNaN]
-      [(subnormal? stored-exp man) (* sign (expt 2 -126) (reify-frac man))]
-      [(zero? stored-exp man) 0]
-      [else (let* ([frac        (reify-frac man)]
+      [(inf? stored-exp mantissa) (cond
+                                    [(equal? 1 sign) POS-INFINITY]
+                                    [(equal? -1 sign) NEG-INFINITY])]
+      [(NaN? stored-exp mantissa) NaN]
+      [else (let* ([frac        (reify-mantissa mantissa)]
                    [shifted-exp (reify-exp stored-exp)])
               (* sign (expt 2 shifted-exp) frac))])))
 
 #|
-(binary-exponent-folder bit acc-pair)
-  bit: A 1 or a 0. int?
-  acc-pair: (k, total) pair?
-
-    k: The current exponent.
-    total: The current binary sum total.
-
-  Given a bit and an accumulator, we return a new accumulator (k+1, (+ total (* bit (expt 2 k)))).
-|#
-(define/match (binary-exponent-folder bit acc-pair)
-  [(0 (cons k total)) (cons (+ k 1) total)]
-  [(1 (cons k total)) (let* ([new-exp (+ 1 k)]
-                             [new-total (+ total (expt 2 k))])
-                        (cons new-exp new-total))]
-  [(_ _) (cons 0 0)])
-
-#|
-(make-binary-folder init-exp)
-  init-exp: defines the intial exponent to be used to recurse over a list.
-
-  Returns a function that takes in a bit-list and returns the value it represents shifted by init-exp.
-|#
-(define (make-exp-shifted-binary-folder init-exp)
-  (lambda (lst)
-    (let* ([acc-pair (foldl binary-exponent-folder (cons init-exp 0) lst)])
-      (cdr acc-pair))))
-
-#|
-(reify-frac man)
-  man: The mantissa bits of a MKFP number.
+(reify-frac mantissa)
+  mantissa: The mantissa bits of a MKFP number.
   Returns the Racket float equivallent of what the mantissa is. 
 |#
-(define (reify-frac man)
-  (let* ([shifted-binary-expander (make-exp-shifted-binary-folder -23)])
-    (shifted-binary-expander man)))
+(define (reify-mantissa mantissa)
+    (reify-bitlist mantissa (+ (- PRECISION) 1)))
+
+#|
+(reify-exp exponent)
+  exp: The exponent bits of a MKFP number.
+  Returns the value of the shifted exponent. 
+|#
+(define (reify-exp exponent)
+    (- (reify-bitlist exponent 0) EXPONENT-SHIFT)) ; StoredExponent - 127
+
 
 #|
 (reify-sign man)
   sgn: The sign bit of a MKFP number.
   Returns 1 if sgn is zero and -1 if sgn is 1. Otherwise FP-ERR is returned. 
 |#
-(define/match (reify-sign sgn)
+(define/match (reify-sign sign)
   [(0) 1]
   [(1) -1]
-  [(_) FP-ERR]
-)
-
+  [(_) FP-ERR])
 
 #|
-(reify-exp exp)
-  exp: The exponent bits of a MKFP number.
-  Returns the value of the shifted exponent. 
+(bitlist-to-int-helper bitlist starting-exponent acc)
+    bitlist: A list of bits in little endian form.
+    LSB-exponent The implicit exponent of the first bit (set to zero to get integers only)
+
+    if bitlist = (b0, b1, b2, ..., bn) and LSB-exponent = s then we return the value of the following
+    sum:
+    (b0*2^s + b1*2^{s+1} + b2*2^{s+2} + ... + bn*2^{s+n})
+
+    Notice that if LSB-exponent = 0 then we return the integer interpretation of the bitlist.
 |#
-(define (reify-exp exp)
-  (let* ([noshift-binary-expander (make-exp-shifted-binary-folder 0)])
-         (+ -127 (noshift-binary-expander exp))))
+(define (reify-bitlist bitlist LSB-exponent)
+    (reify-bitlist-helper bitlist LSB-exponent 0))
+
+#|
+(bitlist-to-int-helper bitlist LSB-exponent acc)
+    bitlist: A list of bits in little endian form.
+    LSB-exponent: The implicit exponent of the first bit (set to zero to get integers only)
+    acc: Accumulator for the sum.
+
+    Tail recursive helper function for converting a list of bits into racket number.
+|#
+(define/match (reify-bitlist-helper bitlist LSB-exponent acc)
+    [((list) _ acc) acc]
+
+    [((cons 0 bitlist-rest) k acc) 
+        (reify-bitlist-helper bitlist-rest (+ k 1) acc)]
+    
+    [((cons 1 bitlist-rest) k acc) 
+        (let* 
+            ([next-k (+ k 1)]
+            [next-acc (+ acc (expt 2 k))])
+
+            (reify-bitlist-helper bitlist-rest next-k next-acc))])
+ 
+
+
 
--- test-reify.rkt
+++ test-reify.rkt
@@ -1,21 +1,17 @@
 #lang racket
 (require rackunit rackunit/text-ui "float.rkt" "numbers.rkt")
 
-(define zero  `(0 ,(list) ,(list)))
-(define one   '(0 (1 1 1 1 1 1 1)   (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 1)))
-(define p42   '(0 (0 0 1 0 0 0 0 1) (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 1  0 1 0 1)))
-(define n421  '(1 (1 1 1 0 0 0 0 1) (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 1  0 1 0 0  1 0 1 1)))
-(define three '(0 (0 0 0 0 0 0 0 1) (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0  0 0 1 1)))
-(define p2^23 `(0 ,(build-num 104) ,(append (make-list 23 0) '(1))))
-(define p12.5     '(0 (0 1 0 0 0 0 0 1) (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 1  0 0 1 1)))
-(define n45.125   '(1 (0 0 1 0 0 0 0 1) (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 1  0 0 1 0  1 1 0 1)))
+(define zero  `(0 ,(list) (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)))
+(define one   '(0 (1 1 1 1 1 1 1)   (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 1)))
+(define p42   '(0 (0 0 1 0 0 0 0 1) (0 0 0 0  0 0 0 0  0 0 0 1  0 1 0 1)))
+(define n421  '(1 (1 1 1 0 0 0 0 1) (0 0 0 0  0 0 0 1  0 1 0 0  1 0 1 1)))
+(define three '(0 (0 0 0 0 0 0 0 1) (0 0 0 0  0 0 0 0  0 0 0 0  0 0 1 1)))
+(define p12.5     '(0 (0 1 0 0 0 0 0 1) (0 0 0 0  0 0 0 0  0 0 0 1  0 0 1 1)))
+(define n45.125   '(1 (0 0 1 0 0 0 0 1) (0 0 0 0  0 0 0 1  0 0 1 0  1 1 0 1)))
 
-(define pinf `(0 (1 1 1 1 1 1 1 1) ,(append (make-list 23 0) '(1))))
-(define ninf `(1 (1 1 1 1 1 1 1 1) ,(append (make-list 23 0) '(1))))
-(define qnan `(0 (1 1 1 1 1 1 1 1) (0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1))); Only most sig bit is important for distinguishing nans
-(define snan `(0 (1 1 1 1 1 1 1 1) (0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1)))
-(define psmallestd `(0 ,(list) (1)))
-(define plargestd `(0 ,(list) ,(make-list 23 1))); 2^-126 * (1-2^(-23))
+(define pinf `(0 (1 1 1 1 1 1 1 1) ,(append (make-list 16 0) '(1))))
+(define ninf `(1 (1 1 1 1 1 1 1 1) ,(append (make-list 16 0) '(1))))
+(define nan `(0 (1 1 1 1 1 1 1 1) (0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1))); Only most sig bit is important for distinguishing nans
 
 (define reify-exp-tests
   (test-suite "Tests for reify-exp"
@@ -24,9 +20,9 @@
               (test-case "Given 0 -> -127"
                          (check = (reify-exp (build-num 0)) -127))
               (test-case "Given 1 -> -126"
-                         (check = (reify-exp (build-num 127)) 0))
+                         (check = (reify-exp (build-num 1)) -126))
               (test-case "Given 254 -> 127"
-                         (check = (reify-exp (build-num 127)) 0))))
+                         (check = (reify-exp (build-num 254)) 127))))
 
 (define reify-sign-tests
   (test-suite "Tests for reify-sign"
@@ -36,21 +32,21 @@
                          (check = (reify-sign 1) -1))))
 
 (define reify-frac-tests
-  (test-suite "Tests for reify-frac"
+  (test-suite "Tests for reify-mantissa"
               (test-case "Given oleg 0 -> 0"
-                         (check = (reify-frac '()) 0))
+                         (check = (reify-mantissa '()) 0))
               (test-case "Given non-oleg 0 -> 0"
-                         (check = (reify-frac '(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)) 0))
-              (test-case "Given '(1)->(expt 2 -23)"
-                         (check = (reify-frac '(1)) (expt 2 -23)))
-              (test-case "Given '(1 1)-> (+ (expt 2 -22) (expt 2 -23))"
-                         (check = (reify-frac '(1 1)) (+ (expt 2 -22) (expt 2 -23))))))
+                         (check = (reify-mantissa '(0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)) 0))
+              (test-case "Given '(1)->(expt 2 -15)"
+                         (check = (reify-mantissa '(1)) (expt 2 -15)))
+              (test-case "Given '(1 1)-> (+ (expt 2 -14) (expt 2 -15))"
+                         (check = (reify-mantissa '(1 1)) (+ (expt 2 -14) (expt 2 -15))))))
 
 (define reify-tests
   (test-suite "Tests for reify"
               (test-case "Given mkfp(1) -> 1"
                          (check = (reify one) 1))
-              (test-case "Given mkfp(0) -> 10"
+              (test-case "Given mkfp(0) -> 0"
                          (check = (reify zero) 0))
               (test-case "Given mkfp(-421) -> -421"
                          (check = (reify n421) -421))
@@ -62,30 +58,16 @@
                          (check = (reify p12.5) 12.5))
               (test-case "Given mkfp(-45.125) -> -45.125"
                          (check = (reify n45.125) -45.125))
-              (test-case "Given mkfp(2^23) -> 2^23"
-                         (check = (reify p2^23) (expt 2 -23)))
-              (test-case "Given mkfp(2^-149) -> 2^-149"
-                         (check = (reify psmallestd) (expt 2 -149)))
-              (test-case "Given mkfp((1-2^(-23))2^-126) -> (1-2^(-23))2^-126"
-                         (check = (reify plargestd) (* (expt 2 -126) (- 1 (expt 2 -23)))))
-              (test-case "Given mkfp(qNaN) -> 'qNaN"
-                         (check equal? (reify qnan) qNaN))
-              (test-case "Given mkfp(sNaN) -> 'sNaN"
-                         (check equal? (reify snan) sNaN))
+              (test-case "Given mkfp(NaN) -> 'NaN"
+                         (check equal? (reify nan) NaN))
               (test-case "Given mkfp(pinf) -> 'positive-infinity"
                          (check equal? (reify pinf) POS-INFINITY))
               (test-case "Given mkfp(ninf) -> 'negative-infinity"
                          (check equal? (reify ninf) NEG-INFINITY))))
 
-(define all-tests (test-suite "all tests" 
-                              reify-exp-tests
-                              reify-sign-tests
-                              reify-frac-tests
-                              reify-tests))
-
 (run-tests reify-exp-tests)
 (run-tests reify-sign-tests)
 (run-tests reify-frac-tests)
 (run-tests reify-tests)
-;(run-tests all-tests)
+
 
