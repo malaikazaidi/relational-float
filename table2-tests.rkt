477f45ae8946f61f976538b4f2db601664905221
Malaika Zaidi <malaika.zaidi@mail.utoronto.ca>
2021-06-11 11:53:40 -0400

    added more exhaustive tests

--- table2-tests.rkt
+++ table2-tests.rkt
@@ -4,8 +4,11 @@
 
 (define table2-test-suite
     (test-suite "Tests for Table 2"
-        (test/fp-relation-r "1 * pi = ?" ((fp-multo one pi x) (x))
-            (check-results-fp-equal? 'results pi ))
+        (test/fp-relation-r "1 * 0 = 0" ((fp-multo one zero x) (x))
+            (check-results-fp-equal? 'results zero ))
+        ;twice to ensure the timing is accurate
+        (test/fp-relation-r "1 * 0 = 0" ((fp-multo one zero x) (x))
+            (check-results-fp-equal? 'results zero ))
         (test/fp-relation-r "1 * pi = ?" ((fp-multo one pi x) (x))
             (check-results-fp-equal? 'results pi ))
         (test/fp-relation-r "pi * 1 = ?" ((fp-multo pi one x) (x))
@@ -14,20 +17,23 @@
             (check-results-fp-equal? 'results p7850 ))
         (test/fp-relation-r "20pi * 125 = ?" ((fp-multo 20pi p125 x) (x))
             (check-results-fp-equal? 'results p7850))
-        (test/fp-relation-r "1 * x = pi" ((fp-multo one x pi) (x))
-            (check-results-fp-equal? 'results pi ))
-        (test/fp-relation-r "x * 1 = pi" ((fp-multo x one pi) (x))
-            (check-results-fp-equal? 'results pi ))
-        (test/fp-relation-r "x * pi = 1" ((fp-multo x pi one) (x))
-            (check-results-fp-equal? 'results pi ))
-        (test/fp-relation-r "20pi * x = 125" ((fp-multo 20pi x p125) (x))
-            (check-results-fp-equal? 'results p7850))
-        (test/fp-relation-r "125 * x = 20pi" ((fp-multo p125 x 20pi) (x))
-            (check-results-fp-equal? 'results p7850 ))
+        (test/fp-relation-r "-2.5 * 3.25 = -8.125" ((fp-multo n2.5 p3.25 x) (x))
+            (check-results-fp-equal? 'results n8.125))
+        (test/fp-relation-r "-2.5 * -3.25 = 8.125" ((fp-multo n2.5 n3.25 x) (x))
+            (check-results-fp-equal? 'results p8.125))
+        (test/fp-relation-r "x * y = pi" ((fp-multo x y pi) (x y))
+            (check-results-fp-equal? 'results pi))
+       
 ))
 
 
             
 (run-tests table2-test-suite)
-(time(run 1 (x y) (fp-multo x y pi))) 
+(time (run 3 (x) (fp-multo x two p40)))
+(time (run 10 (x) (fp-multo x zero zero)))
+(time (run 3 (x y) (fp-multo p20 x p40)))
+(time(run 1 (x y) (fp-multo x y pi)))
+(time(run 3 (x y) (fp-multo x y n9)))
+(time(run 5 (x y) (fp-multo negfour x y))) 
 (time(run 1 (x y z) (fp-multo x y z)))
+
